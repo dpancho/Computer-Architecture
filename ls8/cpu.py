dd25ce419da51d9050d74200f499181fc57b6c57
dpancho <dalepancho7@gmail.com>
2020-05-20 19:17:45 -0700

    day 2 MVP

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -8,8 +8,8 @@ class CPU:
     def __init__(self):
         """Construct a new CPU."""
         self.pc = 0
-        self.reg = [0] * 8 
-        self.ram = [0] * 256 
+        self.reg = [0] * 8     ##8-bit register
+        self.ram = [0] * 256   ##256-bit RAM
 
     def ram_read(self, mar):
         return self.ram[mar]
@@ -17,26 +17,33 @@ class CPU:
     def ram_write(self,mar,mdr):
         self.ram[mar] = mdr
 
-    def load(self):
+    def load(self, file_name):
         """Load a program into memory."""
 
         address = 0
 
+        with open(file_name) as f: 
+            lines = f.readlines()
+            lines = [line for line in lines if line.startswith('0') or line.startswith('1')]
+            program = [int(line[:8], 2) for line in lines]
+
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
+        
+        print(self.ram)
 
 
     def alu(self, op, reg_a, reg_b):
@@ -44,7 +51,10 @@ class CPU:
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        elif op == "SUB": 
+            self.reg[reg_a] -= self.reg[reg_b]
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -70,30 +80,41 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        HLT = 0b00000001
-        LDI = 0b10000010
-        PRN = 0b01000111
+        HLT = 0b00000001  ##Hault = 1
+        LDI = 0b10000010  ##Load  = 130
+        PRN = 0b01000111  ##Print = 71
+        MUL = 0b10100010
 
         running = True
+        IR = self.pc
 
         while running:
-            instruction = self.ram_read(self.pc)
-            opr_a = self.ram_read(self.pc + 1)
-            opr_b = self.ram_read(self.pc + 2)
+            instruction = self.ram_read(IR)
+            opr_a = self.ram_read(IR + 1)
+            opr_b = self.ram_read(IR + 2)
 
+            ##if instruc is Hault
             if instruction == HLT:
-                running = False
-                self.pc +=1
+                # running = False
+                # self.pc +=1
+                sys.exit(0)
 
-
+            ##if instruc is Load
             elif instruction == LDI:
                 self.reg[opr_a] = opr_b
-                self.pc += 3 
+                IR += 3 
 
+            ##if instruc is Print
             elif instruction == PRN:
                 print(self.reg[opr_a])
-                self.pc += 2
+                IR += 2
+
+            elif instruction == MUL:
+                self.alu("MUL", opr_a, opr_b)
+                IR += 3 
+                print('you are MULTIPLYING')
 
             else:
                 print(f"bad input: {bin(instruction)}")
-                running = False
+                # running = False
+                sys.exit(1)
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -7,5 +7,6 @@ from cpu import *
 
 cpu = CPU()
 
-cpu.load()
+# print(sys.argv)
+cpu.load(sys.argv[1])
 cpu.run()
